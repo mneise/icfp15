542ec7a0cdf6d4bb2b5701a000b90ea85f64813d
Felix Geller <fgeller@gmail.com>
2015-08-09 16:09:15 -0500

    adds test back in

--- move_test.go
+++ move_test.go
@@ -506,6 +506,55 @@ func TestPathFindingFailure(t *testing.T) {
 	}
 }
 
+func TestRotateMove(t *testing.T) {
+
+	//  ⬡ ⬡ ⬡ ⬡
+	//   ⬢ · ⬢ ⬡
+	//  ⬡ ⬡ ⬡ ⬡
+
+	u := Unit{Members: []Cell{Cell{0, 1}, Cell{2, 1}}, Pivot: Cell{1, 1}}
+
+	actual := u.Move(RC)
+	expected := Unit{Members: []Cell{Cell{1, 0}, Cell{2, 2}}, Pivot: Cell{1, 1}}
+
+	if !actual.eq(expected) {
+		t.Errorf("incorrect 1st move, got\n%v expected\n%v", actual, expected)
+	}
+
+	//  ⬡ ⬢ ⬡ ⬡
+	//   ⬡ · ⬡ ⬡
+	//  ⬡ ⬡ ⬢ ⬡
+
+	actual = expected.Move(RC)
+	expected = Unit{Members: []Cell{Cell{2, 0}, Cell{1, 2}}, Pivot: Cell{1, 1}}
+
+	if !actual.eq(expected) {
+		t.Errorf("incorrect 2rd move, got\n%v expected\n%v", actual, expected)
+	}
+
+	//  ⬡ ⬡ ⬢ ⬡
+	//   ⬡ · ⬡ ⬡
+	//  ⬡ ⬢ ⬡ ⬡
+
+	actual = expected.Move(RC)
+	expected = Unit{Members: []Cell{Cell{2, 1}, Cell{0, 1}}, Pivot: Cell{1, 1}}
+
+	if !actual.eq(expected) {
+		t.Errorf("incorrect 3rd move, got\n%v expected\n%v", actual, expected)
+	}
+
+	//  ⬡ ⬡ ⬡ ⬡
+	//   ⬢ · ⬢ ⬡
+	//  ⬡ ⬡ ⬡ ⬡
+
+	actual = expected.Move(RCC)
+	expected = Unit{Members: []Cell{Cell{2, 0}, Cell{1, 2}}, Pivot: Cell{1, 1}}
+
+	if !actual.eq(expected) {
+		t.Errorf("incorrect 3rd move, got\n%v expected\n%v", actual, expected)
+	}
+}
+
 func TestRotateMoveWeirdo(t *testing.T) {
 
 	//  · ⬡ ⬢ ⬢
